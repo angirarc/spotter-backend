4638e716be0c3f54e0d3f6d5b3d817f7540d1d62
Reinhardt Angira <reinhardtcollins@live.com>
2025-03-31 09:27:29 +0300

    refactor: cors allow all

--- spotter/settings.py
+++ spotter/settings.py
@@ -58,13 +58,13 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000', 
     'https://spoter-frontend.vercel.app',
